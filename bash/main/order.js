aec6e64e79b0733f82cf337e2757fe1442910745
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2014-09-30 17:52:06 -0400

    fixin some bug

--- bash/main/order.js
+++ bash/main/order.js
@@ -4,10 +4,10 @@
 var path=require('path')
   , fs=require('fs')
   , dirpath=process.argv[2]||'.'
-  , count=process.argv[3]||2
+  , digits=+(process.argv[3])||2
   , ext=process.argv[4]
   , local=path.resolve(dirpath)
-  , pad=function pad(n,len){
+  , pad=function(n,len){
         return (new Array(len+1).join('0')+n).slice(-len)
     }
 
@@ -21,14 +21,13 @@ fs.exists(local,function(exists){
                 var old_path=path.resolve(local,
                              files[i])
                   , new_path=path.resolve(local,
-                             pad(i+1,2)+(ext||path.extname(old_path)))
+                             pad((i+1),digits)+(ext||path.extname(old_path)))
                 console.log(old_path);
                 console.log(new_path);
+                console.log();
                 fs.rename(old_path,new_path,function(err){
                     if(err){
                         console.log('ERROR: '+err);
-                    }else{
-                        console.log('done');
                     }
                 });
             }
