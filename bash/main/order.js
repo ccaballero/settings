8af0c6e4d4c78a1735f245ccec0ce37ee8f1dd52
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2015-03-08 21:46:09 -0400

    added offset for order.js script

--- bash/main/order.js
+++ bash/main/order.js
@@ -6,6 +6,7 @@ var path=require('path')
   , dirpath=process.argv[2]||'.'
   , digits=+(process.argv[3])||2
   , ext=process.argv[4]
+  , offset=+(process.argv[5])||0
   , local=path.resolve(dirpath)
   , pad=function(n,len){
         return (new Array(len+1).join('0')+n).slice(-len)
@@ -17,11 +18,12 @@ fs.exists(local,function(exists){
     }else{
         fs.readdir(local,function(err,files){
             files.sort();
-            for(var i=0; i<files.length; i++){
+            for(var i=1; i<=files.length; i++){
                 var old_path=path.resolve(local,
-                             files[i])
+                             files[i-1])
                   , new_path=path.resolve(local,
-                             pad((i+1),digits)+(ext||path.extname(old_path)))
+                             pad((offset+i),digits)+'.'+
+                             (ext||path.extname(old_path)))
                 console.log(old_path);
                 console.log(new_path);
                 console.log();
