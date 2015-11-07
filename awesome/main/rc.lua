2999c079b3eae5ec387db94425962a05a851f1bc
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2015-11-07 00:27:33 -0400

    settings

--- awesome/main/rc.lua
+++ awesome/main/rc.lua
@@ -38,9 +38,9 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-count = 15
+count = 16
 switch = string.format('%02d',math.floor(math.random()*count)+1)
-switch = '16'
+--switch = '16'
 beautiful.init("/home/jacobian/.config/awesome/themes/" .. switch .. "-theme-1920x1080.lua")
 
 -- This is used later as the default terminal and editor to run.
