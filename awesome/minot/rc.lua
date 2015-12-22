0a2b045910bf7397fd7871fede38d703082c8f2e
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2015-12-22 11:50:22 -0400

    minot settings

--- awesome/minot/rc.lua
+++ awesome/minot/rc.lua
@@ -40,7 +40,7 @@ end
 -- Themes define colours, icons, font and wallpapers.
 count = 16
 switch = string.format('%02d',math.floor(math.random()*count)+1)
-switch = '16'
+--switch = '16'
 beautiful.init("/home/jacobian/.config/awesome/themes/" .. switch .. "-theme-1366x768.lua")
 
 -- This is used later as the default terminal and editor to run.
